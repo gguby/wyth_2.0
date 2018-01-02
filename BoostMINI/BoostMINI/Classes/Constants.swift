7864ba89a70e305d02210c13a64e9978f4687d40
HS Lee <hyungsuk.lee@iriver.com>
2018-01-02 16:36:09 +0900

    Constants, Definitions 정리.

--- BoostMINI/BoostMINI/Classes/Constants.swift
+++ BoostMINI/BoostMINI/Classes/Constants.swift
@@ -12,29 +12,27 @@ import Foundation
 import UIKit
 
 internal class BSTConstants {
-    typealias main  = BSTMainViewContants
+    static let main  = BSTMainViewContants()
 }
 
 internal struct BSTMainViewContants {
 	
 	// 높이는 디바이스에 따라 44가 아닐 수 있다.
-    static let kdNavigationBarHeight: CGFloat = 44 // 53 //add statusbar height
-    static let kdTabBarHeight: CGFloat = 48.0 // 66.0
+    let kdNavigationBarHeight: CGFloat = 44 // 53 //add statusbar height
+    let kdTabBarHeight: CGFloat = 48.0 // 66.0
 //    static let uiThemeMainColor = MLColor.aquamarineColor()
-    static var gKeyboardRect = CGRect.zero
-    static let pageSize = 10
+    var gKeyboardRect = CGRect.zero
+    let pageSize = 10
 	
-	
-	
-	
-	static var appVersion: String {
+	var appVersion: String {
 		let version = ProcessInfo.processInfo.operatingSystemVersion
 		let versionString = "\(version.majorVersion).\(version.minorVersion).\(version.patchVersion)"
 		return versionString
 	}
+}
 
-	
-	
+internal enum PaginationDirection {
+    case Upward, DownWard
 }
 
 internal enum BSTSystem: String {
