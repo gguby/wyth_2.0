fd42112ec246d5271b3f23eba66b2f67316ebfd0
dk(Jack) <raicoll@gmail.com>
2018-01-05 10:45:09 +0900

    인트로 화면의 테스트용 찌끄레기 모두 제거

--- BoostMINI/BoostMINI/Classes/ViewControllers/IntroScreens/IntroViewController.swift
+++ BoostMINI/BoostMINI/Classes/ViewControllers/IntroScreens/IntroViewController.swift
@@ -6,83 +6,19 @@
 // Copyright © 2017 IRIVER LIMITED. All rights reserved.
 //
 
-import Foundation
 import UIKit
-import ReactorKit
-import RxCocoa
-import RxSwift
-
-
-
-
-/// 여기는 대부분 지울 코드들이다.. 흠.
 
 class IntroViewController: UIViewController {
-
-	@IBOutlet weak var tiltingView: TopTiltingView!
-	@IBOutlet var tilts: [TopTiltingView]!
-
-	@IBOutlet weak var slider: UISlider!
-	@IBOutlet weak var testView: UIView!
-
-	@IBOutlet weak var goHome:  UIButton!
-	@IBOutlet weak var goLogin: UIButton!
-	var disposeBag = DisposeBag()
-
-	
-	
-	var isDebugMode = false
 	
 	override func viewDidLoad() {
         super.viewDidLoad()
         checkVersion()
-		slider.minimumValue = -100
-		slider.maximumValue = 100
-	
-		var toggle = false
-		for view in tilts {
-			toggle = !toggle
-			view.useCenter = toggle
-		}
-		tiltingView.useCenter = true
-		
-		
-		
-		goHome.rx.tap.subscribe() { event in
-			self.presentHome()
-			}.disposed(by: disposeBag)
-		goLogin.rx.tap.subscribe() { event in
-			self.presentLogin()
-			}.disposed(by: disposeBag)
-
     }
 
 	override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-	
-	@IBAction func sliderChanged(_ sender: Any) {
-		isDebugMode = true
-
-		let val = self.slider.value.c
-		for view in tilts {
-			view.updateDisplayTiltMask(val)
-		}
-
-	}
-	
-	@IBAction func testButtonTouched(_ sender: Any) {
-	
-		isDebugMode = true
-		
-		// -100 ~ 100
-		let newVal: CGFloat = (arc4random() % 200) - 100
-		for view in tilts {
-			view.updateDisplayTiltMask(newVal, animation: true)
-		}
-	}
-	
 }
 
 extension IntroViewController {
@@ -91,7 +27,7 @@ extension IntroViewController {
 	private func checkVersion() {
 		// 버전을 체크한다.
 		AppVersionModel.get { block in
-			
+
 			// TODO: api 실패해도 우선 진행
 			let block_isSucceed = true	//block.isSucceed
 			if not(block_isSucceed) {
@@ -107,7 +43,8 @@ extension IntroViewController {
 			let forceUpdate = data?.isForceUpdate ?? false
 			let isNeedUpdate = data?.isNeedUpdate ?? false
 			
-			// TODO : 서버 응답없는 경우도 필요하다. 이는 네트워크 에러 핸들링에서 해주게 될 것이다.
+			// TODO : 서버 응답없는 경우도 필요하다. 하지만 이는 네트워크 에러 핸들링에서 해주게 될 것이다.
+			
 			RunInNextMainThread {
 				if isNeedUpdate {
 					self.showUpdateAlert(forceUpdate: forceUpdate)
@@ -119,8 +56,6 @@ extension IntroViewController {
 	}
 	
 	func showUpdateAlert(forceUpdate: Bool = false) {
-		
-		
 		BSTFacade.ux
 			.showAlert( BSTFacade.localizable.alert.updateMessage(),
 					   title: BSTFacade.localizable.alert.updateTitle(),
@@ -152,13 +87,8 @@ extension IntroViewController {
 		if SessionHandler.shared.isLoginned {
 			// 로그인 유저
 			RunInNextMainThread(withDelay: 2.0, {
-				if self.isDebugMode {
-					self.testView.isHidden = false
-					return
-				}
 				self.presentHome()
 			})
-			
 		} else {
 			// 비로그인 유저
 			self.presentLogin()
@@ -167,14 +97,19 @@ extension IntroViewController {
 
 	
 	func presentHome() {
-		let storyboard = UIStoryboard(name: "Home", bundle: nil)
-		let vc = storyboard.instantiateInitialViewController()!	// HomeViewController의 상위 NavigationViewController 이다.
+		guard let vc = R.storyboard.home().instantiateInitialViewController() else {
+			BSTFacade.ux.showToast(BSTFacade.localizable.error.viewControllerMissing("Home.initialViewController"))
+			return
+		}
 		self.present(vc, animated: false, completion: {
 		})
 	}
 	
 	func presentLogin() {
-		let vc = LogInViewController.create("SignUp")
+		guard let vc = R.storyboard.signUp.logInViewController() else {
+			BSTFacade.ux.showToast(BSTFacade.localizable.error.viewControllerMissing("LogInViewController"))
+			return
+		}
 		self.present(vc, animated: false, completion: {
 		})
 	}
