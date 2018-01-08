282a584fc20bc73865bceac216801d0bbdb2a888
KoMyeongbu <kmb@smtown.com>
2018-01-08 17:01:20 +0900

    [kmb] SMTOWN 폰트 추가

--- BoostMINI/BoostMINI/Classes/ViewControllers/HomeScreens/HomeViewController.swift
+++ BoostMINI/BoostMINI/Classes/ViewControllers/HomeScreens/HomeViewController.swift
@@ -62,6 +62,15 @@ class HomeViewController: UIViewController {
         self.initProperties()
         self.initUI()
         self.prepareViewDidLoad()
+       
+        for family: String in UIFont.familyNames
+        {
+            print("\(family)")
+            for names: String in UIFont.fontNames(forFamilyName: family)
+            {
+                print("== \(names)")
+            }
+        }
     }
     
     
