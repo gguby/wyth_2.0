0631c7d2c39f1a6a57aa3345a413747dc2396d4d
dk(Jack) <raicoll@gmail.com>
2018-01-08 14:11:40 +0900

    일단  참고차원정도에서 폴더 참조로 프로젝트에 넣었다. 절대 타겟멤버십에 추가하지말자. 코드로 실행되어서도 안되고, 소스 리소스로 배포되는것도 안된다
    추후 api 변경시, codegenRestAPI.h실행을 통해, Swagger디렉토리내의 파일들의 diff 결과를 통해, api 변경점을 더 빨리 찾아낼 수 있다는 장점이 있을 수 있다.

--- codegenRestAPI.sh
+++ codegenRestAPI.sh
@@ -5,3 +5,7 @@ cd $(dirname "$0")
 # use swift4 + rxswift
 
 swagger-codegen generate -i http://boostdev.lysn.com/v2/api-docs?group=API -l swift4 -c codegen-config.json -o BoostMINI
+
+// Classes 디렉토리보다는 Libraries 디렉토리가 더 어울린다.
+mv -f BoostMINI/BoostMINI/Classes/Swaggers BoostMINI/BoostMINI/Libraries/ 2>>/dev/null
+
