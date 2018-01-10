5b2f7498f50f9ab172b9e28930cf9b9132c856bd
HS Lee <hslee@smtown.com>
2018-01-10 18:18:22 +0900

    바코드 스캔 페이지 추가,

---
+++ BoostMINI/BoostMINI/Classes/ViewControllers/DeviceScreens/TicketScanViewController.swift
@@ -0,0 +1,62 @@
+//
+//  TicketScanViewController.swift
+//  BoostMINI
+//
+//  Created by HS Lee on 10/01/2018.
+//Copyright © 2018 IRIVER LIMITED. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TicketScanViewController: UIViewController {
+
+    //MARK: * properties --------------------
+
+
+    //MARK: * IBOutlets --------------------
+
+
+    //MARK: * Initialize --------------------
+
+    override func viewDidLoad() {
+
+        self.initProperties()
+        self.initUI()
+        self.prepareViewDidLoad()
+    }
+
+
+    private func initProperties() {
+
+    }
+
+
+    private func initUI() {
+
+    }
+
+
+    func prepareViewDidLoad() {
+
+    }
+
+    //MARK: * Main Logic --------------------
+
+
+    //MARK: * UI Events --------------------
+
+
+    //MARK: * Memory Manage --------------------
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+
+extension TicketScanViewController {
+
+}
