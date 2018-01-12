a8ca1d7da52c60eda8831885ecfc75ddc8cc27f2
HS Lee <hslee@smtown.com>
2018-01-12 18:55:31 +0900

    바코드 스캔 화면 수정, 권한 소스 추가,

---
+++ BoostMINI/BoostMINI/Classes/Manager/PermissionManager.swift
@@ -0,0 +1,17 @@
+//
+//  PermissionManager.swift
+//  BoostMINI
+//
+//  Created by HS Lee on 12/01/2018.
+//  Copyright © 2018 IRIVER LIMITED. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Permission
+
+typealias PermissionHandlerCompletion = (PermissionType, PermissionStatus) -> Void
+class PermissionManager {
+
+    
+}
