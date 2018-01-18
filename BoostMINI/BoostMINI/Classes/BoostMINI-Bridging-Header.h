158c13f8764f42803db5103f49610a5f9c16baa4
HS Lee <hslee@smtown.com>
2018-01-18 17:17:40 +0900

    FLEX 코드 수정,

--- BoostMINI/BoostMINI/Classes/BoostMINI-Bridging-Header.h
+++ BoostMINI/BoostMINI/Classes/BoostMINI-Bridging-Header.h
@@ -1,9 +1,10 @@
 //
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
-
 @import RxSwift;
 @import RxCocoa;
 
 //#import "Common.h"
-#import <FLEX/FLEX.h>
+#import <FLEX/FLEXManager.h>
+
+
