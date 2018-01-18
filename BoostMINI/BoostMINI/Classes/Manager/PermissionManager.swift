a5daa1395b7a604b094b002b24fa757ff20a563e
HS Lee <hslee@smtown.com>
2018-01-18 15:12:42 +0900

    퍼미션 매니저 소스 수정,

--- BoostMINI/BoostMINI/Classes/Manager/PermissionManager.swift
+++ BoostMINI/BoostMINI/Classes/Manager/PermissionManager.swift
@@ -12,26 +12,23 @@ import Permission
 
 class PermissionManager {
 
-    class func requestDeterminingPermission(completion: BSTClosure.emptyAction?) {
-        
+    /// 권한 설정을 요청함.
+    class func requestDeterminingPermission(completion: BSTClosure.emptyAction? = nil) {
+       
         let permissionSet = PermissionSet(Permission.defaultSet)
-        var isPermissionRequesting = false
         
         let queue = DispatchQueue.global(qos: .default)
         let dispatchGroup = DispatchGroup()
         
-        for (index, permission) in permissionSet.permissions.enumerated() {
+        for (_, permission) in permissionSet.permissions.enumerated() {
             dispatchGroup.enter()  //  Enter the dispatch group
             
             queue.async(group: dispatchGroup, execute: {
                 if permission.status == .notDetermined {//권한 체크 이력 확인,
-                    isPermissionRequesting = true
-                    
                     permission.request({ (status) in  //각 권한별 요청
                         logDebug(status)
                         dispatchGroup.leave() // Exit dispatch group
                     })
-                    
                 } else {
                     dispatchGroup.leave() // Exit dispatch group
                 }
@@ -39,6 +36,19 @@ class PermissionManager {
             
             dispatchGroup.notify(queue: DispatchQueue.main, execute: {
                 completion?()
+            })
+        }
+    }
+    
+    ///앱 설정 화면을 로딩함.
+    class func openAppPermissionSettings() {
+        guard let settingsUrl = URL(string: UIApplicationOpenSettingsURLString) else {
+            return
+        }
+        
+        if UIApplication.shared.canOpenURL(settingsUrl) {
+            UIApplication.shared.open(settingsUrl, completionHandler: { (success) in
+                logDebug("Settings opened: \(success)") // Prints true
             })
         }
     }
