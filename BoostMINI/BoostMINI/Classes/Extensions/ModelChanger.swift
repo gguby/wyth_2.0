45726f652bb181e55d5296db644f3b4ee74f1d03
dk(Jack) <raicoll@gmail.com>
2018-01-19 21:05:55 +0900

    안쓰게 되어진 클래스. (일단은 삭제하지 않고 보류. 쓰고싶으면 상속받아서 사용하시면 됩니다. 프로퍼티들의 이름과 타입이 일치하는 값들이 있으면 자동으로 변환이 됩니다)

--- BoostMINI/BoostMINI/Classes/Extensions/ModelChanger.swift
+++ BoostMINI/BoostMINI/Classes/Extensions/ModelChanger.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 
-
+///  NoticeViewModel의 삭제와 관련해서 얘도 철거 위기에 놓임. 이미 파일명과 클래스명이 다른데 방치중.
+class NoticeViewModel : EasyCodable {
+	//static var apiList: [String : APIRequest]
+	}
 open class EasyCodable: Codable {
 	
 	/// 다른 모델에서 현재 모델로 변경
