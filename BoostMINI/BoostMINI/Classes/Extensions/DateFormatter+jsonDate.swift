e3db02c999962159d2c7067157b5686a357286d3
dk(Jack) <raicoll@gmail.com>
2018-01-23 10:21:58 +0900

    날짜처리 우선순위 변경.

--- BoostMINI/BoostMINI/Classes/Extensions/DateFormatter+jsonDate.swift
+++ BoostMINI/BoostMINI/Classes/Extensions/DateFormatter+jsonDate.swift
@@ -9,12 +9,13 @@
 import UIKit
 
 // .formatted(DateFormatter.jsonDate)
+// json string에서 regex 돌려서 포맷이 있는쪽을 우선으로 돌리는게 성능은 더 나올 수 있다. 일단 yyyy-MM-dd'T'HH:mm:ss.SSS 로
 
 extension DateFormatter {
 	static let jsonDate: DateFormatter = {
 		let formatter = DateFormatter()
 		// ISO8601DateFormatter
-		formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+		formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS"
 		formatter.calendar = Calendar(identifier: .iso8601)
 		formatter.timeZone = TimeZone(identifier: "KST")
 		//formatter.timeZone = TimeZone(secondsFromGMT: 0)
@@ -25,7 +26,7 @@ extension DateFormatter {
 	// createAt 등등
 	static let jsonDate2: DateFormatter = {
 		let formatter = DateFormatter()
-		formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS"
+		formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
 		formatter.calendar = Calendar(identifier: .iso8601)
 		//formatter.timeZone = TimeZone(identifier: "KST")
 		formatter.timeZone = TimeZone(secondsFromGMT: 0)
@@ -35,7 +36,7 @@ extension DateFormatter {
 
 	static let jsonDate3: DateFormatter = {
 		let formatter = DateFormatter()
-		formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
+		formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
 		formatter.calendar = Calendar(identifier: .iso8601)
 		//formatter.timeZone = TimeZone(identifier: "KST")
 		formatter.timeZone = TimeZone(secondsFromGMT: 0)
@@ -45,7 +46,7 @@ extension DateFormatter {
 	
 	static let jsonDate4: DateFormatter = {
 		let formatter = DateFormatter()
-		formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+		formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
 		formatter.calendar = Calendar(identifier: .iso8601)
 		//formatter.timeZone = TimeZone(identifier: "KST")
 		formatter.timeZone = TimeZone(secondsFromGMT: 0)
